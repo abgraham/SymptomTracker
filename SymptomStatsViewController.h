4286b7b0174b03338fed95eb1aa4d6312ce41bdd
Annie Graham <abgraham@stanford.edu>
2016-10-28 19:08:05 +0200

    Adds SymptomStatsViewController to select and view stats for filtered symptoms

---
+++ SymptomStatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SymptomStatsViewController.h
+//  SymptomTracker
+//
+//  Created by Annie Graham on 10/28/16.
+//  Copyright © 2016 Annie Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SymptomStatsViewController : UIViewController
+
+@end
---
+++ SymptomStatsViewController.m
@@ -0,0 +1,13 @@
+//
+//  SymptomStatsViewController.m
+//  SymptomTracker
+//
+//  Created by Annie Graham on 10/28/16.
+//  Copyright © 2016 Annie Graham. All rights reserved.
+//
+
+#import "SymptomStatsViewController.h"
+
+@implementation SymptomStatsViewController
+
+@end
