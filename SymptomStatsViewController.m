1aa4621c17fa20b80073b05b74e7841c84e8c53e
Annie Graham <abgraham@stanford.edu>
2016-10-28 19:24:12 +0200

    Adds outlets and parsing for filters in SymptomStats page

--- SymptomStatsViewController.m
+++ SymptomStatsViewController.m
@@ -8,6 +8,37 @@
 
 #import "SymptomStatsViewController.h"
 
+@interface SymptomStatsViewController () {
+
+    __weak IBOutlet UIDatePicker *toDatePicker;
+    __weak IBOutlet UIDatePicker *fromDatePicker;
+    __weak IBOutlet UITextField *fromSeverity;
+    __weak IBOutlet UITextField *toSeverity;
+}
+@end
+
 @implementation SymptomStatsViewController
+
+- (void)viewDidLoad {
+
+}
+
+- (IBAction)showStatsPressed:(id)sender {
+    NSDate *fromDate = fromDatePicker.date;
+    NSDate *toDate = toDatePicker.date;
+    NSInteger fromSeverityValue = [fromSeverity.text integerValue];
+    NSInteger toSeverityValue = [toSeverity.text integerValue];
+    NSLog(@"FROM AND TO: %ld %ld", fromSeverityValue, toSeverityValue);
+    //[self showFoodGroupCount:foodGroupCount];
+    //[self showTimeOfDayCount:timeOfDayCount];
+}
+
+- (void)showFoodGroupCount:(NSDictionary *)foodGroupCount {
+
+}
+
+- (void)showTimeOfDayCount:(NSDictionary *)timeOfDayCount {
+    NSString *stringRepresentation;
+}
 
 @end
