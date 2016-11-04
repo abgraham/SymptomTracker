2f676e0907386ac0f28f3fac96d7af6bae1ef2ec
Annie Graham <abgraham@stanford.edu>
2016-11-04 13:35:08 +0100

    Adds saveSymptoms and updates init in PersistencyManager to use archiving

--- PersistencyManager.h
+++ PersistencyManager.h
@@ -17,5 +17,6 @@
 - (NSArray *)symptomsWithSeverity:(NSString *)severity;
 - (NSArray *)symptomsWithBodyPart:(NSString *)bodyPart;
 - (void)addSymptomWithSeverity:(NSInteger)severity location:(NSString *)location foodGroups:(NSArray *)foodGroups date:(NSDate *)date;
+- (void)saveSymptoms;
 
 @end
--- PersistencyManager.m
+++ PersistencyManager.m
@@ -25,23 +25,29 @@
     self = [super init];
     if (self){
 
-        NSDateComponents* tomorrowComponents = [NSDateComponents new] ;
-        tomorrowComponents.hour = -12;
-        NSCalendar* calendar = [NSCalendar currentCalendar];
-        NSDate *now = [NSDate date];
-        NSDate* tomorrow = [calendar dateByAddingComponents:tomorrowComponents toDate:now options:0] ;
-        NSDateComponents* yesterdayComponents = [NSDateComponents new] ;
-        yesterdayComponents.hour = -6;
-        NSDate* yesterday = [calendar dateByAddingComponents:yesterdayComponents toDate:now options:0] ;
+        NSData *data = [NSData dataWithContentsOfFile:[NSHomeDirectory() stringByAppendingString:@"/Documents/symptoms.bin"]];
+        symptoms = [NSKeyedUnarchiver unarchiveObjectWithData:data];
 
-        Symptom *symptom1 = [[Symptom alloc] initWithTime:yesterday severity:2 bodyPart:@"stomach" foodGroups:[NSArray arrayWithObjects:@"dairy", @"gluten", @"fiber", nil]];
-        Symptom *symptom2 = [[Symptom alloc] initWithTime:tomorrow severity:3 bodyPart:@"chin" foodGroups:[NSArray arrayWithObjects: @"dairy", @"gluten", nil]];
-        Symptom *symptom3 = [[Symptom alloc] initWithTime:[NSDate date] severity:4 bodyPart:@"chin" foodGroups:[NSArray arrayWithObjects: @"dairy", @"gluten", @"eggs", nil]];
-        Symptom *symptom4 = [[Symptom alloc] initWithTime:[NSDate date] severity:5 bodyPart:@"chin" foodGroups:[NSArray arrayWithObjects: @"dairy", @"gluten", @"eggs", nil]];
+        if (symptoms == nil) {
 
-        symptoms = [NSMutableArray arrayWithObjects:symptom1, symptom2, symptom3, symptom4, nil];
+            NSDateComponents* tomorrowComponents = [NSDateComponents new] ;
+            tomorrowComponents.hour = -12;
+            NSCalendar* calendar = [NSCalendar currentCalendar];
+            NSDate *now = [NSDate date];
+            NSDate* tomorrow = [calendar dateByAddingComponents:tomorrowComponents toDate:now options:0] ;
+            NSDateComponents* yesterdayComponents = [NSDateComponents new] ;
+            yesterdayComponents.hour = -6;
+            NSDate* yesterday = [calendar dateByAddingComponents:yesterdayComponents toDate:now options:0] ;
 
-        foodGroups = [NSArray arrayWithObjects:@"gluten", @"dairy", @"eggs", @"high-fiber", @"meat", @"high-fat", nil];
+            Symptom *symptom1 = [[Symptom alloc] initWithTime:yesterday severity:2 bodyPart:@"stomach" foodGroups:[NSArray arrayWithObjects:@"dairy", @"gluten", @"fiber", nil]];
+            Symptom *symptom2 = [[Symptom alloc] initWithTime:tomorrow severity:3 bodyPart:@"chin" foodGroups:[NSArray arrayWithObjects: @"dairy", @"gluten", nil]];
+            Symptom *symptom3 = [[Symptom alloc] initWithTime:[NSDate date] severity:4 bodyPart:@"chin" foodGroups:[NSArray arrayWithObjects: @"dairy", @"gluten", @"eggs", nil]];
+            Symptom *symptom4 = [[Symptom alloc] initWithTime:[NSDate date] severity:5 bodyPart:@"chin" foodGroups:[NSArray arrayWithObjects: @"dairy", @"gluten", @"eggs", nil]];
+
+            symptoms = [NSMutableArray arrayWithObjects:symptom1, symptom2, symptom3, symptom4, nil];
+
+            foodGroups = [NSArray arrayWithObjects:@"gluten", @"dairy", @"eggs", @"high-fiber", @"meat", @"high-fat", nil];
+        }
     }
 
     return self;
@@ -108,6 +114,12 @@
 
 - (void)addSymptomWithSeverity:(NSInteger)severity location:(NSString *)location foodGroups:(NSArray *)symptomFoodGroups date:(NSDate *)date{
     [symptoms addObject:[[Symptom alloc] initWithTime:date severity:severity bodyPart:location foodGroups:symptomFoodGroups]];
+}
+
+- (void)saveSymptoms {
+    NSString *fileName = [NSHomeDirectory() stringByAppendingString:@"/Documents/symptoms.bin"];
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:symptoms];
+    [data writeToFile:fileName atomically:YES];
 }
 
 @end
