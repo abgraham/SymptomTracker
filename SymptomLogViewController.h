7daeb3faacb49b689de1e99444701396cef9c3d4
Annie Graham <abgraham@stanford.edu>
2016-12-29 11:04:28 -0800

    Adds symptom log view controller

---
+++ SymptomLogViewController.h
@@ -0,0 +1,13 @@
+//
+//  SymptomLogViewController.h
+//  SymptomTracker
+//
+//  Created by Annie Graham on 12/29/16.
+//  Copyright © 2016 Annie Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SymptomLogViewController : UIViewController
+
+@end
